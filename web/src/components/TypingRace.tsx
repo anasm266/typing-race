7403d817d9651b6aca0b3ef7a213b610dc56f673
Anas <kingzcopz266@gmail.com>
2026-04-20 08:55:25 -0700

    Revert "fix: support mobile keyboard input"
    This reverts commit 0f87cf51ac9c61ab344593f24aaa8034e5c24ca9.

--- web/src/components/TypingRace.tsx
+++ web/src/components/TypingRace.tsx
@@ -4,7 +4,6 @@ import { useTyping } from "../hooks/useTyping";
 import { randomPassage, type Passage } from "../lib/passages";
 import { CapsLockWarning } from "./CapsLockWarning";
 import { Passage as PassageView } from "./Passage";
-import { TouchKeyboardInput } from "./TouchKeyboardInput";
 import { Stats } from "./Stats";
 import { ResultScreen } from "./ResultScreen";
 
@@ -77,13 +76,6 @@ export function TypingRace() {
       <CapsLockWarning visible={capsLockOn} />
 
       <PassageView passage={passage.text} typed={typing.typed} />
-
-      <TouchKeyboardInput
-        typed={typing.typed}
-        canFocus={true}
-        canType={true}
-        onKey={handleKey}
-      />
 
       <div className="flex flex-col items-center gap-3">
         <div className="text-xs text-fg-dimmer flex gap-4">
