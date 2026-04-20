1d2ef201083534710526caf1a4b7e45cb3b87e8f
Anas <kingzcopz266@gmail.com>
2026-04-20 08:55:25 -0700

    Revert "fix: support mobile keyboard input"
    This reverts commit 3ec9490bfa05d563444640f46d14066f6feaf4eb.

--- web/src/components/RaceView.tsx
+++ web/src/components/RaceView.tsx
@@ -6,7 +6,6 @@ import { Passage } from "./Passage";
 import { EndScreen } from "./EndScreen";
 import { ReactionBar } from "./ReactionBar";
 import { ReactionToast } from "./ReactionToast";
-import { TouchKeyboardInput } from "./TouchKeyboardInput";
 import { calcAccuracy, formatElapsed, type WpmSample } from "../lib/wpm";
 import type {
   ClientMsg,
@@ -224,13 +223,6 @@ export function RaceView({
       />
 
       <CapsLockWarning visible={capsLockOn} />
-
-      <TouchKeyboardInput
-        typed={typing.typed}
-        canFocus={status === "starting" || status === "racing"}
-        canType={status === "racing" && typing.state !== "done"}
-        onKey={typing.handleKey}
-      />
 
       <ReactionBar send={send} />
 
